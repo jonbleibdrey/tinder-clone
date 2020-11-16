3b5f91c4ca275aa2197315cd15430698b588af58
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-11-16 16:33:10 -0500

    finished header

--- src/Header.js
+++ src/Header.js
@@ -3,16 +3,20 @@ import "./Header.css"
 import FaceIcon from '@material-ui/icons/Face';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import Flag from "./assest/images/flag2.PNG";
+import IconButton from "@material-ui/core/IconButton";
 
 
 function Header() {
     return (
         <div className="header">
-            <FaceIcon/>
+            <IconButton>
+            <FaceIcon fontSize="large" className="header__icon"/>
+            </IconButton>
 
             <img className="header__logo" src={Flag} alt="america flag"/>
-
-            <MailOutlineIcon/>
+            <IconButton>
+            <MailOutlineIcon fontSize="large" className="header__icon"/>
+            </IconButton>
         </div>
     )
 }
