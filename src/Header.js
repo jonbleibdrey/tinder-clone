0509f0329cda1cf6fd2179d06528338233ac6455
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-11-16 16:20:26 -0500

    created logo and connected icons

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import "./Header.css"
 import FaceIcon from '@material-ui/icons/Face';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import Flag from "./assest/images/flag2.PNG";
+
 
 function Header() {
     return (
-        <div>
-            <FaceIcon />
-            <h2>i am a header</h2>
+        <div className="header">
+            <FaceIcon/>
+
+            <img className="header__logo" src={Flag} alt="america flag"/>
+
+            <MailOutlineIcon/>
         </div>
     )
 }
