26f00aca8668aeac5aa25f80e90fc4d5808b49a2
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-11-17 14:31:51 -0500

    updated firebase

--- src/Header.js
+++ src/Header.js
@@ -4,18 +4,19 @@ import FaceIcon from '@material-ui/icons/Face';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import Flag from "./assest/images/flag2.PNG";
 import IconButton from "@material-ui/core/IconButton";
+import { blue } from '@material-ui/core/colors';
 
 
 function Header() {
     return (
         <div className="header">
             <IconButton>
-            <FaceIcon fontSize="large" className="header__icon"/>
+            <FaceIcon style={{ color: blue[800], fontSize: 65 }} className="header__icon"/>
             </IconButton>
 
             <img className="header__logo" src={Flag} alt="america flag"/>
             <IconButton>
-            <MailOutlineIcon fontSize="large" className="header__icon"/>
+            <MailOutlineIcon style={{ color: blue[800], fontSize: 65  }} className="header__icon"/>
             </IconButton>
         </div>
     )
