c2949cb24eda3eb969cb67843aa1bfd859ca4f65
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-11-18 12:24:05 -0500

    created icons and added them to be clickable

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -4,14 +4,29 @@ import ReplayIcon from '@material-ui/icons/Replay';
 import CloseIcon from '@material-ui/icons/Close';
 import StarRateIcon from '@material-ui/icons/StarRate';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-
+import FlashOnIcon from '@material-ui/icons/FlashOn';
+import IconButton from '@material-ui/core/IconButton';
 
 import "./SwipeButtons.css"
 
 function SwipeButtons() {
     return (
         <div className="swipeButtons">
-            <h2>Buttons go here!</h2>
+            <IconButton>
+            <ReplayIcon fontSize="large" />
+            </IconButton>
+            <IconButton>
+            <CloseIcon fontSize="large" />
+            </IconButton>
+            <IconButton>
+            <StarRateIcon fontSize="large" />
+            </IconButton>
+            <IconButton>
+            <FavoriteIcon fontSize="large" />
+            </IconButton>
+            <IconButton>
+            <FlashOnIcon fontSize="large" />
+            </IconButton>
         </div>
     )
 }
