3eaecf92eb27e026f9972a822f86a8b7137d1e3e
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-11-18 12:45:26 -0500

    finshed buttons

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -12,21 +12,27 @@ import "./SwipeButtons.css"
 function SwipeButtons() {
     return (
         <div className="swipeButtons">
-            <IconButton>
+
+            <IconButton className="swipeButtons__repeat">
             <ReplayIcon fontSize="large" />
             </IconButton>
-            <IconButton>
+
+            <IconButton className="swipeButtons__left">
             <CloseIcon fontSize="large" />
             </IconButton>
-            <IconButton>
+
+            <IconButton className="swipeButtons__star">
             <StarRateIcon fontSize="large" />
             </IconButton>
-            <IconButton>
+
+            <IconButton className="swipeButtons__right">
             <FavoriteIcon fontSize="large" />
             </IconButton>
-            <IconButton>
+
+            <IconButton className="swipeButtons__lightning">
             <FlashOnIcon fontSize="large" />
             </IconButton>
+            
         </div>
     )
 }
